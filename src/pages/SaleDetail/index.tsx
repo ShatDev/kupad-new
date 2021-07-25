55bf732d666143917066869e4f2ebe91cdf28b4e
fromif <ryanhee@yahoo.com>
2021-07-25 14:44:40 +1000

    fixed

--- src/pages/SaleDetail/index.tsx
+++ src/pages/SaleDetail/index.tsx
@@ -10,6 +10,7 @@ import useClaimed from "hook/useClaimed";
 import useParticipants from "hook/useParticipants";
 import useSigner from "hook/useSigner";
 import Logo from "assets/images/logos/kubridge.png";
+import KupadLogo from "assets/images/logos/favicon.png";
 import { toBigNumber, toHumanNumber } from "utils/formatter";
 import {
   hardCap,
@@ -126,7 +127,9 @@ export default () => {
   return (
     <div className="kpd-sale-details">
       <div className="container">
-        <h3 className="kpd-sale-details-title">Kupad Token Sale</h3>
+        <h3 className="kpd-sale-details-title topSpacing100">
+          Kupad Token Sale
+        </h3>
         <div className="kpd-sale-details-card">
           {startTime > Date.now() / 1000 && (
             <span className="up-coming">Up-Coming</span>
@@ -135,7 +138,7 @@ export default () => {
             <span className="up-coming">Active</span>
           )}
           {startTime <= Date.now() / 1000 && isFinished && (
-            <span className="up-coming">Ended</span>
+            <span className="up-coming">Up-Coming</span>
           )}
           <div className="info">
             <div className="logo-container">
@@ -190,6 +193,211 @@ export default () => {
             <div className="item">
               <span className="desc">No of participants</span>
               <span className="value">TBA</span>
+            </div>
+            <div className="item no-border">
+              <span className="desc">Access</span>
+              <span className="value">Whitelist</span>
+            </div>
+            <span className="raise-amount">
+              {toHumanNumber(totalInvest)}/{toHumanNumber(presaleTarget)} KCS
+            </span>
+            <div className="total-progress">
+              <div
+                className="progress"
+                style={{
+                  width: `${
+                    Number(
+                      totalInvest
+                        .mul(BigNumber.from("10000"))
+                        .div(presaleTarget)
+                        .toNumber()
+                    ) / 100
+                  }%`,
+                }}
+              />
+            </div>
+            {active && account && !whitelisted && <span>Not whitelisted</span>}
+            {active && account && whitelisted && (
+              <>
+                {!isFinished && (
+                  <div className="buy-container">
+                    <input
+                      className="amount-input"
+                      value={amount}
+                      placeholder="0.0"
+                      type="number"
+                      onChange={(e) => setAmount(e.currentTarget.value)}
+                    />
+                    <span>KCS</span>
+                    <button className="btn-connect" onClick={setMax}>
+                      Max
+                    </button>
+                    {startTime > Date.now() / 1000 && (
+                      <button className="btn-connect">Not started</button>
+                    )}
+                    {startTime <= Date.now() / 1000 && (
+                      <button className="btn-connect" onClick={invest}>
+                        Buy
+                      </button>
+                    )}
+                  </div>
+                )}
+                {isFinished && !claimed && (
+                  <div className="buy-container">
+                    <span>
+                      {toHumanNumber(
+                        presaleSupply
+                          .mul(totalInvest)
+                          .div(presaleTarget)
+                          .mul(invested)
+                          .div(totalInvest)
+                      )}{" "}
+                      KUPAD allocated
+                    </span>
+                    {canClaim && (
+                      <button className="btn-connect" onClick={claim}>
+                        Claim
+                      </button>
+                    )}
+                  </div>
+                )}
+                {isFinished && claimed && (
+                  <div className="buy-container">
+                    <span>
+                      {toHumanNumber(
+                        presaleSupply
+                          .mul(totalInvest)
+                          .div(presaleTarget)
+                          .mul(invested)
+                          .div(totalInvest)
+                      )}{" "}
+                      KUPAD claimed
+                    </span>
+                  </div>
+                )}
+              </>
+            )}
+
+            <div className="footer">
+              {active && account ? (
+                <button className="btn-connect" onClick={deactivate}>
+                  Disconnect
+                </button>
+              ) : (
+                <button className="btn-connect" onClick={actionHandler}>
+                  Connect Wallet
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className="kpd-sale-details-token-info">
+          <h3 className="title">Token Information</h3>
+          <div className="kpd-sale-details-card">
+            <div className="item">
+              <span className="desc">Token Symbol</span>
+              <span className="value">TBA</span>
+            </div>
+            <div className="item">
+              <span className="desc">Token Name</span>
+              <span className="value">TBA</span>
+            </div>
+            <div className="item">
+              <span className="desc">Access Type</span>
+              <span className="value">Whitelist</span>
+            </div>
+            <div className="item">
+              <span className="desc">Contract</span>
+              <a
+                href="https://explorer.kcc.io/en/token/0x85eb19c76a1cd461ecb62b0f97fdf67cbef41c1a"
+                target="__blank"
+                className="value"
+              >
+                https://explorer....
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="kpd-sale-details-token-info">
+          <h3 className="title">About project</h3>
+          <h5 className="title-mid">What is Kubridge Finance?</h5>
+          <span className="desc">
+            The first cross-chain bridge on the Kucoin Community Chain(KCC).
+            KuBridge is a revolution project unlike any that has been seen in
+            the Kucoin Community Chain because it has the potential to
+            seamlessly “Merge” KCC with other blockchains and this is essential
+            for the growth and development of Blockchain and decentralization.
+          </span>
+        </div>
+      </div>
+
+      <div className="container">
+        <h3 className="kpd-sale-details-title">Kupad Token Sale</h3>
+        <div className="kpd-sale-details-card">
+          {startTime > Date.now() / 1000 && (
+            <span className="up-coming">Up-Coming</span>
+          )}
+          {startTime <= Date.now() / 1000 && !isFinished && (
+            <span className="up-coming">Active</span>
+          )}
+          {startTime <= Date.now() / 1000 && isFinished && (
+            <span className="up-coming">Ended</span>
+          )}
+          <div className="info">
+            <div className="logo-container">
+              <img src={KupadLogo} alt="Kupad Logo" />
+            </div>
+            <div className="meta">
+              <p className="title">KUPAD</p>
+              <ul className="kpd-footer-social">
+                <li>
+                  <a href="https://kubridge.finance/">
+                    <i className="fa fa-globe" />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://t.me/KuBridgeKcc">
+                    <i className="fas fa-paper-plane" />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/KuBridgeKCC">
+                    <i className="fab fa-twitter" />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/KuBridgeKCC">
+                    <i className="fab fa-medium-m" />
+                  </a>
+                </li>
+                {/* <li>
+                  <a href="/">
+                    <img src={Reddit} alt="Reddit" />
+                  </a>
+                </li> */}
+              </ul>
+            </div>
+          </div>
+          <div className="status">
+            <div className="item">
+              <span className="desc">Start in</span>
+              <span className="value">Jun 22, 2021 5:00 PM</span>
+            </div>
+            <div className="item">
+              <span className="desc">Max Allocation</span>
+              <span className="value">39.85 / 39.85 KCS</span>
+            </div>
+            {active && account && (
+              <div className="item">
+                <span className="desc">Your Investment</span>
+                <span className="value">{toHumanNumber(invested)} KCS</span>
+              </div>
+            )}
+            <div className="item">
+              <span className="desc">No of participants</span>
+              <span className="value">100</span>
             </div>
             <div className="item no-border">
               <span className="desc">Access</span>
